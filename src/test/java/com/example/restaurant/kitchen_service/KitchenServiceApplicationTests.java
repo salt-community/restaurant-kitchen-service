7c71ef27b52bded308115d234817e454eda0cec8
samuel-kar <samuel.karlhager@hotmail.com>
2025-08-29 14:19:09 +0200

    init commit

---
+++ src/test/java/com/example/restaurant/kitchen_service/KitchenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant.kitchen_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
