cf129dfccc7f295f4e54d7646244fe761fd5dd41
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-02 10:42:17 +0200

    add logic to LoggingTicketService

--- src/main/java/com/example/restaurant/kitchen_service/service/LoggingTicketService.java
+++ src/main/java/com/example/restaurant/kitchen_service/service/LoggingTicketService.java
@@ -1,4 +1,40 @@
 package com.example.restaurant.kitchen_service.service;
 
-public class LoggingTicketService implements TicketAppService {
+import com.example.restaurant.kitchen_service.kafka.dto.OrderCanceledEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.OrderCreatedEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.PaymentAuthorizedEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.PaymentFailedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class LoggingTicketService implements TicketAppService {
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingTicketService.class);
+
+    @Override
+    public void onOrderCreated(OrderCreatedEvent event) {
+        LOG.info("[ticket] order.created received -> create/queue ticket for orderId={} items={}",
+                event.orderId(), event.items());
+        //TODO fix create and store KitchenTicket in DB
+    }
+
+    @Override
+    public void onOrderCanceled(OrderCanceledEvent event) {
+        LOG.info("[ticket] order.canceled received -> cancel ticket for orderId={} reason={}",
+                event.orderId(), event.reason());
+        // TODO: cancel if not handed over, produce kitchen.canceled
+    }
+
+    @Override
+    public void onPaymentAuthorized(PaymentAuthorizedEvent event) {
+        LOG.info("[ticket] payment.authorized received -> evaluate hours/capacity for orderId={}",
+                event.orderId());
+        // TODO: accept or cancel(reason=CLOSED|CAPACITY), produce event
+    }
+
+    @Override
+    public void onPaymentFailed(PaymentFailedEvent event) {
+        LOG.info("[ticket] payment.failed received -> cancel ticket for orderId={} reason={}",
+                event.orderId(), event.reason());
+        // TODO: cancel(reason=PAYMENT_FAILED), produce event
+    }
 }
