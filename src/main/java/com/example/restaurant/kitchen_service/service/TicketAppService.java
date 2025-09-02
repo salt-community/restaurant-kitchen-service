a16ac292c8297fbd987b8cf64cd1857a4838fb4d
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-02 10:35:44 +0200

    add temporary services for setting up consumers

---
+++ src/main/java/com/example/restaurant/kitchen_service/service/TicketAppService.java
@@ -0,0 +1,13 @@
+package com.example.restaurant.kitchen_service.service;
+
+import com.example.restaurant.kitchen_service.kafka.dto.OrderCanceledEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.OrderCreatedEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.PaymentAuthorizedEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.PaymentFailedEvent;
+
+public interface TicketAppService {
+    void onOrderCreated(OrderCreatedEvent event);
+    void onOrderCanceled(OrderCanceledEvent event);
+    void onPaymentAuthorized(PaymentAuthorizedEvent event);
+    void onPaymentFailed(PaymentFailedEvent event);
+}
