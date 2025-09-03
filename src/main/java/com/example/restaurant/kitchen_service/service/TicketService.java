96d7443db7fa35a567625601aa18c3164bddddff
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-03 09:18:40 +0200

    add ticketservice interface

---
+++ src/main/java/com/example/restaurant/kitchen_service/service/TicketService.java
@@ -0,0 +1,22 @@
+package com.example.restaurant.kitchen_service.service;
+
+import com.example.restaurant.kitchen_service.kafka.dto.OrderCanceledEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.OrderCreatedEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.PaymentAuthorizedEvent;
+import com.example.restaurant.kitchen_service.kafka.dto.PaymentFailedEvent;
+
+import java.util.UUID;
+
+public interface TicketService {
+    void onOrderCreated(OrderCreatedEvent event);
+    void onOrderCanceled(OrderCanceledEvent event);
+    void onPaymentAuthorized(PaymentAuthorizedEvent event);
+    void onPaymentFailed(PaymentFailedEvent event);
+
+    //for rest
+    void start(UUID ticketId);
+    void ready(UUID ticketId);
+    void handOver(UUID ticketId);
+    void cancel(UUID ticketId, String reason);
+    void delay(UUID ticketId, int minutes, String note);
+}
