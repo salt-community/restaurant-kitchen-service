4d84ae7918189f029570766b79d283c70a291167
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-04 21:10:47 +0200

    clean old smoke test files to avoid confusion

--- src/main/java/com/example/restaurant/kitchen_service/service/TicketAppService.java
+++
@@ -1,9 +0,0 @@
-package com.example.restaurant.kitchen_service.service;
-
-import com.example.restaurant.kitchen_service.kafka.dto.OrderCanceledEvent;
-import com.example.restaurant.kitchen_service.kafka.dto.PaymentAuthorizedEvent;
-
-public interface TicketAppService {
-    void onOrderCanceled(OrderCanceledEvent event);
-    void onPaymentAuthorized(PaymentAuthorizedEvent event);
-}
