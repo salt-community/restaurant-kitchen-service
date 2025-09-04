d5b66984d102b4ac0c1da5c50534aef1eccfde9b
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-04 12:16:15 +0200

    creating Recipe entity

---
+++ src/main/java/com/example/restaurant/kitchen_service/model/Recipe.java
@@ -0,0 +1,28 @@
+package com.example.restaurant.kitchen_service.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "recipe_ingredients",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "ingredient_id")
+    )
+    private List<Ingredient> ingredients;
+}
