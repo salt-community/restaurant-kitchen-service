fa3324ea7d4dd2036020c672e57175fbcd2cea4c
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-04 16:44:44 +0200

    add field

--- src/main/java/com/example/restaurant/kitchen_service/kafka/dto/KitchenEvent.java
+++ src/main/java/com/example/restaurant/kitchen_service/kafka/dto/KitchenEvent.java
@@ -2,9 +2,12 @@ package com.example.restaurant.kitchen_service.kafka.dto;
 
 import com.example.restaurant.kitchen_service.enums.TicketStatus;
 
+import java.time.Instant;
+
 public interface KitchenEvent {
     String eventId();
     String ticketId();
     String orderId();
     TicketStatus status();
+    Instant occurredAt();
 }
