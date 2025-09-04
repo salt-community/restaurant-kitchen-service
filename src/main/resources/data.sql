f5f45d0bdf01ef7bb3cb97d75c126a4b61d6792b
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-04 12:13:44 +0200

    initialising some ingredients

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,21 @@
-INSERT INTO ingredients (id, available_quantity, name) VALUES (1, 1, 'test');
+-- Hamburger ingredients (1-5 units)
+INSERT INTO ingredients (id, available_quantity, name) VALUES (1, 3, 'Beef Patty');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (2, 5, 'Burger Bun');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (3, 4, 'Lettuce');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (4, 2, 'Tomato');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (5, 3, 'Cheese Slice');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (6, 1, 'Onion');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (7, 4, 'Pickles');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (8, 2, 'Bacon');
+
+-- Pizza ingredients (1-5 units)
+INSERT INTO ingredients (id, available_quantity, name) VALUES (9, 1, 'Pizza Dough');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (10, 5, 'Tomato Sauce');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (11, 4, 'Mozzarella Cheese');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (12, 3, 'Pepperoni');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (13, 2, 'Mushrooms');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (14, 1, 'Bell Peppers');
+INSERT INTO ingredients (id, available_quantity, name) VALUES (15, 5, 'Olives');
+
+-- Drinks (1-5 units)
+INSERT INTO ingredients (id, available_quantity, name) VALUES (16, 5, 'Soda');
