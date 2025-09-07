26e45c6aafb4c03301b2bd146addf20378ceadc7
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-07 17:02:06 +0200

    trim unused method

--- src/main/java/com/example/restaurant/kitchen_service/repository/TicketRepository.java
+++ src/main/java/com/example/restaurant/kitchen_service/repository/TicketRepository.java
@@ -1,10 +1,8 @@
 package com.example.restaurant.kitchen_service.repository;
 
-import com.example.restaurant.kitchen_service.enums.TicketStatus;
 import com.example.restaurant.kitchen_service.model.KitchenTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,5 +10,4 @@ public interface TicketRepository extends JpaRepository<KitchenTicket, UUID> {
 
     //if we later enable multiple tickets per order, we can change this optional to a list
     Optional<KitchenTicket> findByOrderId(String orderId);
-    List<KitchenTicket> findByStatus(TicketStatus status);
 }
