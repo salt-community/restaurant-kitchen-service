80d28fe3d6eb2c24f2c2e4e2cf78584b78488144
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-07 17:00:57 +0200

    finish with logic, consider changing pool size from 2 to something bigger but should work for this small app

--- src/main/java/com/example/restaurant/kitchen_service/config/SchedulerConfig.java
+++ src/main/java/com/example/restaurant/kitchen_service/config/SchedulerConfig.java
@@ -3,11 +3,18 @@ package com.example.restaurant.kitchen_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 //used for the autopilot with timers, drop these once we have functional frontend for user, staff, etc
 @Configuration
 public class SchedulerConfig {
 
     @Bean
-    public TaskScheduler taskScheduler() {}
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("kitchen-auto-");
+        scheduler.initialize();
+        return scheduler;
+    }
 }
