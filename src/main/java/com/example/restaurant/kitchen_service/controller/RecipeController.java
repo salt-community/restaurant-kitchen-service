36e008688db4be30287d68e36efaf129019a523f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-08 16:10:53 +0200

    controller to return possible recipes working correctly

--- src/main/java/com/example/restaurant/kitchen_service/controller/RecipeController.java
+++ src/main/java/com/example/restaurant/kitchen_service/controller/RecipeController.java
@@ -4,12 +4,16 @@ import com.example.restaurant.kitchen_service.dto.response.PossibleRecipe;
 import com.example.restaurant.kitchen_service.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api/recipe")
+@RestController
+@RequestMapping("/api/recipe")
+@CrossOrigin
 public class RecipeController {
 
     RecipeService service;
@@ -20,7 +24,7 @@ public class RecipeController {
     }
 
     @GetMapping("/getAllRecipes")
-    ResponseEntity<List<PossibleRecipe>> getAllRecipes() {
-        service.getPossibleRecipes();
+    public ResponseEntity<List<PossibleRecipe>> getAllRecipes() {
+        return ResponseEntity.ok(service.getPossibleRecipes());
     }
 }
