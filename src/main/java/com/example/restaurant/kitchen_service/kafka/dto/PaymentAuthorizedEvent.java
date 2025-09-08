1962ae420e796513370bec2d559fac0c5cd1b8d5
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-08 09:23:28 +0200

    adapging payment authorized dto to receive recipes

--- src/main/java/com/example/restaurant/kitchen_service/kafka/dto/PaymentAuthorizedEvent.java
+++ src/main/java/com/example/restaurant/kitchen_service/kafka/dto/PaymentAuthorizedEvent.java
@@ -3,10 +3,12 @@ package com.example.restaurant.kitchen_service.kafka.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.Instant;
+import java.util.List;
 
 public record PaymentAuthorizedEvent(
         String orderId,
         String paymentId,
-        @JsonFormat(shape = JsonFormat.Shape.STRING) Instant occurredAt
+        @JsonFormat(shape = JsonFormat.Shape.STRING) Instant occurredAt,
+        List<Integer> orderedRecipesIds
 ) {
 }
