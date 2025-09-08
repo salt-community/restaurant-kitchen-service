4e0e968b36059c2c1abf74aa3b527d2cf687ce20
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-08 15:43:14 +0200

    define recipe controller

---
+++ src/main/java/com/example/restaurant/kitchen_service/controller/RecipeController.java
@@ -0,0 +1,25 @@
+package com.example.restaurant.kitchen_service.controller;
+
+import com.example.restaurant.kitchen_service.dto.response.AllRecipesDto;
+import com.example.restaurant.kitchen_service.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/api/recipe")
+public class RecipeController {
+
+    RecipeService service;
+
+    @Autowired
+    public RecipeController(RecipeService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAllRecipes")
+    ResponseEntity<List<AllRecipesDto>> getAllRecipes() {
+    }
+}
