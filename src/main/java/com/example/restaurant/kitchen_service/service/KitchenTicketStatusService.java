186d806ce1d1418deb240d1218e8acf033b35c68
samuel-kar <samuel.karlhager@hotmail.com>
2025-09-08 16:58:44 +0200

    begin to implement logic in status service

--- src/main/java/com/example/restaurant/kitchen_service/service/KitchenTicketStatusService.java
+++ src/main/java/com/example/restaurant/kitchen_service/service/KitchenTicketStatusService.java
@@ -1,4 +1,20 @@
 package com.example.restaurant.kitchen_service.service;
 
+import com.example.restaurant.kitchen_service.repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KitchenTicketStatusService {
+
+    private final TicketRepository repo;
+
+
+    public KitchenTicketStatusService(TicketRepository repo) {
+        this.repo = repo;
+    }
+
+
+
+
+
 }
